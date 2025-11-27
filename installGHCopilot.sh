775c8ff0708d5f712cd9b4cd67e0882dafb1578c
Peter Clemens <pclemens365@gmail.com>
2025-11-26 19:27:16 -0600

    this actually worked

---
+++ installGHCopilot.sh
@@ -0,0 +1,3 @@
+    export EXTENSIONS_GALLERY='{"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "itemUrl": "https://marketplace.visualstudio.com/items"}'
+    code-server --install-extension github.copilot
+    code-server --install-extension github.copilot-chat
